c45affb801d9dedc100414fcec01efdff8c40108
aOlmo <aolmoher@uccs.edu>
2018-03-12 12:43:59 +0100

    Added new Network configuration

--- model.py
+++ model.py
@@ -33,6 +33,8 @@ dummy_y = np_utils.to_categorical(encoded_Y)
 def baseline_model():
     model = Sequential()
     model.add(Dense(4, input_dim=4, init='normal', activation='relu'))
+    model.add(Dense(10, init='normal', activation='relu'))
+    model.add(Dense(10, init='normal', activation='relu'))
     model.add(Dense(3, init='normal', activation='sigmoid'))
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
