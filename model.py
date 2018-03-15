12c0fb28fdcb80963a49ff5a48dfdda7310cc997
aOlmo <aolmoher@uccs.edu>
2018-03-15 18:01:56 +0100

    Improved model
    - The program now splits the dataset in train and test sets
    - We get the accuracy and losses

--- model.py
+++ model.py
@@ -2,46 +2,66 @@ import numpy
 import pandas
 from keras.models import Sequential
 from keras.layers import Dense
-from keras.wrappers.scikit_learn import KerasClassifier
 from keras.utils import np_utils
 from sklearn.cross_validation import cross_val_score
 from sklearn.cross_validation import KFold
 from sklearn.preprocessing import LabelEncoder
 from sklearn.pipeline import Pipeline
 
+from sklearn.model_selection import train_test_split
+
+import os
+
+weights_name = "weights.hdf5"
 numpy.random.seed(0)
 
-dataframe   = pandas.read_csv("iris.csv", header=None)
-dataset     = dataframe.values
+dataframe = pandas.read_csv("iris.csv", header=None)
+dataset = dataframe.values
 
 X = dataset[:,0:4].astype(float)
 Y = dataset[:,4]
 
-# Preprocess the labels
+X_train, X_test, y_train, y_test = \
+          train_test_split(X, Y, test_size=0.2, random_state=42)
 
-# LabelEncoder from scikit-learn turns each text label
-# (e.g "Iris-setosa", "Iris-versicolor") into a vector
-# In this case, each of the three labels are just assigned
-# a number from 0-2.
+# Preprocess the labels
 encoder = LabelEncoder()
-encoder.fit(Y)
-encoded_Y = encoder.transform(Y)
+encoder.fit(y_train)
+encoded_y_train = encoder.transform(y_train)
+
+encoder.fit(y_test)
+encoded_y_test = encoder.transform(y_test)
 
 # to_categorical converts the numbered labels into a one-hot vector
-dummy_y = np_utils.to_categorical(encoded_Y)
+y_train = np_utils.to_categorical(encoded_y_train)
+y_test = np_utils.to_categorical(encoded_y_test)
 
-def baseline_model():
-    model = Sequential()
-    model.add(Dense(4, input_dim=4, init='normal', activation='relu'))
-    model.add(Dense(10, init='normal', activation='relu'))
-    model.add(Dense(10, init='normal', activation='relu'))
-    model.add(Dense(3, init='normal', activation='sigmoid'))
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model = Sequential()
+model.add(Dense(4, input_dim=4, init='normal', activation='relu'))
+model.add(Dense(10, activation='relu'))
+model.add(Dense(10, activation='relu'))
+model.add(Dense(3, activation='sigmoid'))
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-    return model
+model.summary()
 
-estimator = KerasClassifier(build_fn=baseline_model, nb_epoch=200, batch_size=5, verbose=0)
+if not os.path.isfile(weights_name):
+  model.fit(X_train, y_train, epochs=200, batch_size=5)
+  model.save_weights(weights_name)
+else:
+  model.load_weights(weights_name)
 
-kfold = KFold(n=len(X), n_folds=10, shuffle=True, random_state=0)
-results = cross_val_score(estimator, X, dummy_y, cv=kfold)
-print("Baseline: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
+# Explicar qué es cross validation
+# Crear una grafica aquí
+# from sklearn.cross_validation import cross_val_score
+# from sklearn.cross_validation import KFold
+
+# model.predict()
+
+score = model.evaluate(X_test, y_test)
+print("Loss:", score[0])
+print("Accuracy: {}%".format(score[1] * 100))
+
+# kfold = KFold(n=len(X), n_folds=10, shuffle=True, random_state=0)
+# results = cross_val_score(estimator, X, dummy_y, cv=kfold)
+# print("Baseline: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
